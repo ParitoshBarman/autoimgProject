50f6cf4361b7a027b948cb7013c2bbe40e1bcbf4
paritoshpuitta <paritoshpuitta@gmail.com>
2023-11-01 16:43:12 +0530

    for render

--- autoimgProject/settings.py
+++ autoimgProject/settings.py
@@ -100,7 +100,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 # Add for Vercel Deployment
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-MEDIA_URLS ='/media/'
+MEDIA_URLS ='media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
